be5f4acbf38a9a7f504efd2298cb9d4e6f2f13ec
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-05-18 21:17:01 +0700

    Add files via upload

---
+++ OnlineHacking/logo.sh
@@ -0,0 +1,56 @@
+clear
+echo -e "\e[95m 
+         ██████╗ ███╗   ██╗██╗     ██╗███╗   ██╗███████╗    
+        ██╔═══██╗████╗  ██║██║     ██║████╗  ██║██╔════╝    
+        ██║   ██║██╔██╗ ██║██║     ██║██╔██╗ ██║█████╗      
+        ██║   ██║██║╚██╗██║██║     ██║██║╚██╗██║██╔══╝      
+        ╚██████╔╝██║ ╚████║███████╗██║██║ ╚████║███████╗    
+         ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝    
+                                                            
+     ██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗   
+     ██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝   
+     ███████║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗  
+     ██╔══██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║  
+     ██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝  
+     ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝  \e[95;1m \e[0m\n"
+    echo""    
+  echo -e $'\e[1;33m\e[0m\e[1;31m    ██████████\e[0m'"\e[1;37m██████████"'\e[1;33m\e[0m\e[0;32m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m Camera Hacking \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.1] \e[0m'                                       
+  echo ""
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github : \e[36mhttps://github.com/OnlineHacKing/CameraHack\e[0m \e[1;32m\e[0m\e[1;33m]\e[0m'
+   echo ""
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[93m    |O|n|l|i|n|e| |H|a|c|k|i|n|g|"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m' 
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[95m    |S|U|M|A|N|"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m' 
+      echo "" 
+sleep 8
+clear
+xdg-open "https://www.onlinehacking.in/hack-front-camera-target-phone-using-termux-linux"
+echo ""
+echo ""
+printf "\e[31m[\e[32m★\e[31m]\e[32m This Tool is only educational purpose and we are not responsible for kind of illegal activity done by this tool\e[m "
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Accepted This Notice \e[1;91m (Y/N) : \e[0m' option
+echo""
+echo""
+echo""
+
+if [[ $option == *'N'* ]]; then
+clear
+exit
+fi
+if [[ $option == *'n'* ]]; then
+clear
+exit
+fi
+
+clear
+
+echo ""
+printf "\e[100;330m[\e[10m **** ]\e[1;40m\e[10m OnlineHacking :\e[1;32m Join Telegram Channel \e[1;33m @OnlineHacking  !\e[0m"
+sleep 4
+echo ""
+clear
