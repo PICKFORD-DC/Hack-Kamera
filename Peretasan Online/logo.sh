946b35bc00d059f97b4283bce3a2c31544c4aabe
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-05-18 16:22:18 +0700

    Add files via upload

---
+++ Peretasan Online/logo.sh
@@ -0,0 +1,48 @@
+clear
+echo -e "\e[95m 
+
+     ████    ████  ████████  ████████  ██  ████
+     ████    ████  ██    ██  ██        ████
+     ████████████  ████████  ██        ████
+     ████    ████  ██    ██  ████████  ██  ████
+    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━•••
+      ████████  ████████  █████  █████  ██████  ███████  ████████
+      ██    ██  ██    ██  ██   ██   ██  ██      ██   ██  ██    ██
+      ██        ████████  ██   ██   ██  ██████  ██████   ████████
+      ██    ██  ██    ██  ██   ██   ██  ██      ██   ██  ██    ██
+      ████████  ██    ██  ██        ██  ██████  ██   ██  ██    ██ BY:PICKFORD
+    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━•••\e[95;1m \e[0m\n"
+    
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github : \e[36mhttps://github.com/OnlineHacKing/CameraHack\e[0m \e[1;32m\e[0m\e[1;33m]\e[0m'
+  echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Facebook : \e[36mYa\e[0m \e[1;32m\e[0m\e[1;33m]\e[0m'
+  echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Whatsapp : \e[36mJangan Kepo Kontol\e[0m \e[1;32m\e[0m\e[1;33m]\e[0m' 
+      echo "" 
+sleep 8
+clear
+xdg-open "https://www.facebook.com/profile.php?id=100077142458119"
+echo ""
+echo ""
+printf "\e[31m[\e[32m★\e[31m]\e[32m GUNAKAN ALAT INI DENGAN SEBIJAK BIJAK NYA JIKALAU ADA MASALAH PRIBADI YANG DI TIMBULKAN ALAT INI AUTHOR TIDAK BERTANGGUNG JAWAB\e[m "
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m PERSETUJUAN \e[1;91m (Y/N) : \e[0m' option
+echo""
+echo""
+echo""
+
+if [[ $option == *'N'* ]]; then
+clear
+exit
+fi
+if [[ $option == *'n'* ]]; then
+clear
+exit
+fi
+
+clear
+
+echo ""
+sleep 4
+echo ""
+clear
+
