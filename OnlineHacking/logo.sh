b2de143da5821174f94964d22661f7dfd84108a3
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-05-19 13:41:57 +0700

    Add files via upload

---
+++ OnlineHacking/logo.sh
@@ -0,0 +1,50 @@
+clear
+echo -e "\e[95m 
+      ██████ ██ ████  ██  ██
+      ██  ██ ██ ██    ██ ██        ██       ██
+      ██████ ██ ██    ████        ████     ████
+      ██     ██ ██    ██ ██        ██       ██
+      ██     ██ ████  ██  ██            ██
+
+
+          ██████   █                 ██
+          ██       ██                ██
+          ██████  ████  ██ ████  ██████
+          ██     ██  ██ ████    ██   ██
+          ██     ██  ██ ██      ██   ██
+          ██      ████  ██       ██████ \e[95;1m \e[0m\n"
+  echo -e $' \e[1;32m ───────────────────────────────────────────────────────────────────────────────────────────────•'
+   echo -e $'\e[1;33m\e[0m\e[1;33m  •⌯•\e[0m\e[1;32m Github   : \e[36mhttps://github.com/PICKFORD-DC\e[0m \e[1;32m\e[0m\e[1;33m]\e[0m'
+  echo -e $'\e[1;33m\e[0m\e[1;33m  •⌯•\e[0m\e[1;32m Facebook  : \e[36mhttps://www.facebook.com/profile.php?id=100077142458119 \e[0m \e[1;32m\e[0m\e[1;33m]\e[0m'
+   echo -e $'\e[1;33m\e[0m\e[1;33m  •⌯•\e[0m\e[1;32m Whatsapp : \e[36mGak Usah Kepo Kontol\e[0m \e[1;32m\e[0m\e[1;33m]\e[0m'
+  echo -e $' \e[1;32m ───────────────────────────────────────────────────────────────────────────────────────────────•'
+      echo "" 
+sleep 8
+clear
+xdg-open "https://www.facebook.com/profile.php?id=100077142458119"
+echo ""
+echo ""
+printf "\e[31m[\e[32m★\e[31m]\e[32m Script Ini Bersifat Ilegal Jikalau Anda Mendapatkan Masalah Pribadi Saat menggunakan Script Ini Saya Sebagai Author Tidak Bertanggung Jawab \e[m "
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Persetujuan \e[1;91m (Y/N) : \e[0m' option
+echo""
+echo""
+echo""
+
+if [[ $option == *'N'* ]]; then
+clear
+exit
+fi
+if [[ $option == *'n'* ]]; then
+clear
+exit
+fi
+
+clear
+
+echo ""
+printf "\e[100;330m[\e[10m **** ]\e[1;40m\e[10m PickFord :\e[1;32m Jangan Lupa Follow Akun Fb Author \e[1;33m Pengen Jadi Seleb bang:V\e[0m"
+sleep 4
+echo ""
+clear
